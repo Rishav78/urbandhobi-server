8933893c836f2f11d7e83fee0cba129e2d184286
Rishav Garg <rishavgarg789@gmail.com>
2021-04-16 00:43:08 +0530

    setup rabbitmq

--- gatekeeper-ms/src/main.ts
+++ gatekeeper-ms/src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
+import './lib/env';
+
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL],
+        queue: 'gate_keeper_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await app.listen(() => {
+    console.log('GateKeeper microservice start listening');
+  });
 }
 bootstrap();
