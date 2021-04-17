818b12857090da868b625bf3d64497b8128c505f
Rishav Garg <rishavgarg789@gmail.com>
2021-04-17 13:46:30 +0530

    implemented API for microservices

--- gatekeeper/src/api/v1/controllers/auth.controller.ts
+++ gatekeeper/src/api/v1/controllers/auth.controller.ts
@@ -1,14 +1,40 @@
-import { Body, Controller, Inject, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Put,
+  Headers,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { handleMSError } from 'src/lib/helpers';
 import { ValidationPipe } from 'src/lib/pipe/validation.pipe';
-import { SignUpWithEmailDTO } from '../dto/auth.dto';
+import { SignInWithEmailDTO, SignUpWithEmailDTO } from '../dto/auth.dto';
 
 @Controller('')
 export class AuthController {
   constructor(
     @Inject('AUTHENTICATION_SERVICE') private readonly authClient: ClientProxy,
   ) {}
+
+  @Post(['signin', 'signin/email'])
+  public async signInWithEmail(
+    @Body(new ValidationPipe()) { email, password }: SignInWithEmailDTO,
+  ) {
+    try {
+      const token = await this.authClient
+        .send('UD.GateKeeper.SignInWithEmail', {
+          email,
+          password,
+        })
+        .toPromise<string>();
+      return token;
+    } catch (error) {
+      throw handleMSError(error);
+    }
+  }
 
   @Put(['signup', 'signup/email'])
   public async signUpWithEmail(
@@ -24,6 +50,26 @@ export class AuthController {
       return id;
     } catch (error) {
       throw handleMSError(error);
+    }
+  }
+
+  @Get('authenticated')
+  public async authenticated(@Headers('Authorization') auth: string) {
+    try {
+      if (!auth) {
+        throw new UnauthorizedException('unauthorized');
+      }
+      const [bearer, token] = auth.split(' ');
+      if (bearer !== 'Bearer' || !token) {
+        throw new UnauthorizedException('unauthorized');
+      }
+      const authenticated: boolean = await this.authClient
+        .send('UD.GateKeeper.IsAuthenticated', token)
+        .toPromise();
+
+      return authenticated;
+    } catch (error) {
+      throw error;
     }
   }
 }
--- gatekeeper/src/api/v1/dto/auth.dto.ts
+++ gatekeeper/src/api/v1/dto/auth.dto.ts
@@ -1,9 +1,34 @@
-import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
 
-export class SignUpWithEmailDTO {
+export class SignInWithEmailDTO {
+  @ApiProperty({ description: '', required: true })
   @IsEmail({}, { message: 'provide a valid email address' })
   public readonly email: string;
 
+  @ApiProperty({ description: '', required: true })
   @IsString({ message: 'password not provided' })
+  public readonly password: string;
+}
+
+export class SignUpWithEmailDTO {
+  @ApiProperty({ description: '', required: true })
+  @IsEmail({}, { message: 'provide a valid email address' })
+  public readonly email: string;
+
+  @ApiProperty({ description: '', required: true })
+  @IsString({ message: 'password must be a string' })
+  @MinLength(8, { message: 'password must be of minimum 8 length' })
+  @MaxLength(20, { message: 'password can have max 20 charcter' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'password must contain a 1 small and capital character, 1 number, 1 special character',
+  })
   public readonly password: string;
 }
