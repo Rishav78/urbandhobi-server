ce80cd1372a323cd6a48f51a33c416124f32cdf2
Rishav Garg <rishavgarg789@gmail.com>
2021-04-17 13:50:59 +0530

    api url structuring

--- gatekeeper/src/app.module.ts
+++ gatekeeper/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RouterModule, Routes } from 'nest-router';
 import { AuthModule } from './api/v1/modules';
-import { V1Module } from './api/v1/v1.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 const routes: Routes = [
   {
-    path: 'v1',
+    path: 'api/auth/v1',
     children: [AuthModule],
   },
 ];
