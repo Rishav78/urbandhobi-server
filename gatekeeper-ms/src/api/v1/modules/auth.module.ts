98131ab845bb7971c02f98daa6cf15fc3e1633ab
Rishav Garg <rishavgarg789@gmail.com>
2021-04-18 19:15:02 +0530

    user rabbitmq

--- gatekeeper-ms/src/api/v1/modules/auth.module.ts
+++ gatekeeper-ms/src/api/v1/modules/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TokenManager } from 'src/lib/utils/token-manager';
 import { AuthEmailController } from '../controllers';
@@ -6,7 +7,22 @@ import { EmailSchema, name } from '../db/models';
 import { AuthEmailService } from '../services';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name, schema: EmailSchema }])],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: 'ud_user_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+    MongooseModule.forFeature([{ name, schema: EmailSchema }]),
+  ],
   controllers: [AuthEmailController],
   providers: [AuthEmailService, TokenManager],
 })
