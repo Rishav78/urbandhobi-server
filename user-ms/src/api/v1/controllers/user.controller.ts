ff19f771ca94d2d40f65a955c2002a39956db346
Rishav Garg <rishavgarg789@gmail.com>
2021-04-20 23:43:38 +0530

    added missing FindByEmail event

--- user-ms/src/api/v1/controllers/user.controller.ts
+++ user-ms/src/api/v1/controllers/user.controller.ts
@@ -34,4 +34,21 @@ export class UserController {
       channel.ack(originalMsg);
     }
   }
+
+  @MessagePattern('UD.User.FindByEmail')
+  public async findByEmail(
+    @Payload(ValidationPipe) { email }: CreateUserDTO,
+    @Ctx() context: RmqContext,
+  ) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    try {
+      const user = await this.userService.findByEmail(email);
+      return user;
+    } catch (error) {
+      throw error;
+    } finally {
+      channel.ack(originalMsg);
+    }
+  }
 }
