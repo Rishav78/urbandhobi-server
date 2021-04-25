3f3a12b9c0dd8d8232382e63c59e33fd0615a18f
Rishav Garg <rishavgarg789@gmail.com>
2021-04-25 14:22:07 +0530

    [cart-dev] integrate winston logger with cart API

--- cart/src/main.ts
+++ cart/src/main.ts
@@ -1,10 +1,15 @@
 import './lib/env';
 
 import { NestFactory } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
+
 import { AppModule } from './app.module';
+import { loggerConfig } from './core/logger/logger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger(loggerConfig),
+  });
   await app.listen(3000);
 }
 bootstrap();
