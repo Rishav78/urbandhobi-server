3015ae47c835071faf89365601a01fa3e868e226
Rishav Garg <rishavgarg789@gmail.com>
2021-04-26 14:00:39 +0530

    [service-dev] basic setup

---
+++ services-ms/src/api/v1/db/entitys/service.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Service {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { nullable: false })
+  name: string;
+
+  @Column('text', { nullable: true })
+  imageId: string;
+
+  @Column('boolean', { nullable: true })
+  active: boolean;
+
+  @Column('uuid', { nullable: false })
+  categoryId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
