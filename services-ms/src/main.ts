364f191976d85a05001a4bed33347b9ea7f0be63
Rishav Garg <rishavgarg789@gmail.com>
2021-05-01 12:33:40 +0530

    [services-dev] integrate winston logger

--- services-ms/src/main.ts
+++ services-ms/src/main.ts
@@ -3,6 +3,8 @@ import './lib/env';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
+import { loggerConfig } from './core/logger/logger.config';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -18,6 +20,7 @@ async function bootstrap() {
           durable: false,
         },
       },
+      logger: WinstonModule.createLogger(loggerConfig),
     },
   );
   await app.listen(() => {
