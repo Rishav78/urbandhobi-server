9faf6881ae25c5d1979b0b253e1b4351d88899d8
Rishav Garg <rishavgarg789@gmail.com>
2021-05-02 20:23:14 +0530

    [service-dev] added seed data for service location

--- services-ms/src/api/v1/db/entitys/index.ts
+++ services-ms/src/api/v1/db/entitys/index.ts
@@ -1,2 +1,3 @@
 export * from './service.entity';
 export * from './service-type.entity';
+export * from './service-area.entity';
---
+++ services-ms/src/data/service-area/state.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    name: 'punjab',
+    countryId: 1,
+    image: 'https://timesofindia.indiatimes.com/img/78886323/Master.jpg',
+  },
+  {
+    id: 2,
+    name: 'odisha',
+    countryId: 1,
+    image: 'https://hindi.nativeplanet.com/img/2018/03/6-1522306498.jpg',
+  },
+];
